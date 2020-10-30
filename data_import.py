9ef39df16ff84d356cf4cc4e161816a345765293
hsaafan <65576707+hsaafan@users.noreply.github.com>
2020-10-30 19:53:23 -0400

    Moved from csv input to yaml

--- data_import.py
+++ data_import.py
@@ -1,26 +1,30 @@
-import csv
+import yaml
+import os
 
 
-def import_from_csv(path):
+def import_from_yaml(path: str, item_type: str):
     item_list = []
-    with open(path, newline='') as csvfile:
-        reader = csv.reader(csvfile)
-        headers = reader.__next__()
-        for row in reader:
-            item = {}
-            for i in range(len(headers)):
-                item[headers[i]] = row[i]
-            item_list.append(item)
+    id_counter = 1
+    for yaml_file in os.scandir(path):
+        if (yaml_file.path.endswith('.yaml')) and yaml_file.is_file():
+            item_file = yaml.load(open(yaml_file, 'r'), Loader=yaml.SafeLoader)
+            for key, item in item_file.items():
+                if item["type"].lower() == item_type:
+                    item["id"] = f"{id_counter:02}"
+                    item_list.append(item)
+                    id_counter += 1
+        else:
+            print(f"Skipping {yaml_file}, not a yaml file")
     return(item_list)
 
 
-def import_products(path='products.csv'):
-    product_list = import_from_csv(path)
+def import_products(path='products'):
+    product_list = import_from_yaml(path, 'product')
     return(product_list)
 
 
-def import_machines(path='machines.csv'):
-    machine_list = import_from_csv(path)
+def import_machines(path='machines'):
+    machine_list = import_from_yaml(path, 'machine')
     return(machine_list)
 
 
--- main.py
+++ main.py
@@ -15,20 +15,18 @@ def main():
 
         var_dict = {}
         for item in products:
-            item_id = f"{int(item['id']):02}"
             # Decision variable
-            var_dict['x' + item_id] = m.addVar(vtype=GRB.INTEGER)
-            var_dict['x' + item_id].varName = 'x' + item_id
+            var_dict['x' + item['id']] = m.addVar(vtype=GRB.INTEGER)
+            var_dict['x' + item['id']].varName = 'x' + item['id']
             # Surplus variable for demand
-            var_dict['y+' + item_id] = m.addVar(vtype=GRB.CONTINUOUS)
-            var_dict['y+' + item_id].varName = 'y+' + item_id
+            var_dict['y+' + item['id']] = m.addVar(vtype=GRB.CONTINUOUS)
+            var_dict['y+' + item['id']].varName = 'y+' + item['id']
             # Slack variable for demand
-            var_dict['y-' + item_id] = m.addVar(vtype=GRB.CONTINUOUS)
-            var_dict['y-' + item_id].varName = 'y-' + item_id
+            var_dict['y-' + item['id']] = m.addVar(vtype=GRB.CONTINUOUS)
+            var_dict['y-' + item['id']].varName = 'y-' + item['id']
         for item in machines:
-            item_id = f"{int(item['id']):02}"
-            var_dict['k' + item_id] = m.addVar(vtype=GRB.BINARY)
-            var_dict['k' + item_id].varName = 'k' + item_id
+            var_dict['k' + item['id']] = m.addVar(vtype=GRB.BINARY)
+            var_dict['k' + item['id']].varName = 'k' + item['id']
 
         # Some function definitions to help out with objective and constraints
         def get_demand(i):
@@ -37,36 +35,31 @@ def main():
         def get_volumes():
             vol = 0
             for item in products:
-                i = f"{int(item['id']):02}"
-                vol += var_dict['x'+i] * item['volume']
+                vol += var_dict['x' + item['id']] * item['volume']
             return(vol)
 
         def get_max_volume():
             vol = 0
             for item in machines:
-                i = f"{int(item['id']):02}"
-                vol += var_dict['k'+i] * item['volume']
+                vol += var_dict['k' + item['id']] * item['volume']
             return(vol)
 
         def get_machines():
             total = 0
             for item in machines:
-                i = f"{int(item['id']):02}"
-                total += var_dict['k'+i]
+                total += var_dict['k' + item['id']]
             return(total)
 
         def get_machine_cost():
             cost = 0
             for item in machines:
-                i = f"{int(item['id']):02}"
-                cost += var_dict['k'+i] * item['cost']
+                cost += var_dict['k' + item['id']] * item['cost']
             return(cost)
 
         def get_demand_slack_penalty():
             penalty = 0
             for item in products:
-                i = f"{int(item['id']):02}"
-                penalty += var_dict['y-'+i] * item['profit']
+                penalty += var_dict['y-' + item['id']] * item['profit']
             return(penalty)
 
         # Objective
@@ -79,9 +72,8 @@ def main():
         # Constraints
         # Demand
         for item in products:
-            item_id = f"{int(item['id']):02}"
-            m.addConstr(get_demand(item_id) == float(item['demand']),
-                        "demand"+item_id)
+            m.addConstr(get_demand(item['id']) == float(item['demand']),
+                        "demand"+item['id'])
         # Volume
         m.addConstr(get_volumes() - get_max_volume() <= 0, 'volume')
         # One Machine
