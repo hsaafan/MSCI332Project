67c0bba94e77846c926a16bb3e7d029edbed4f80
hsaafan <65576707+hsaafan@users.noreply.github.com>
2020-10-31 00:04:14 -0400

    implemented profit constraint

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ from gurobipy import GRB
 
 import data_import
 
+alpha = 10
+
 
 def main():
     try:
         # Create model
         m = gp.Model("mip1")
+        m.setParam("NonConvex", 2)
 
         # Add variables
         products = data_import.import_products()
@@ -24,13 +27,24 @@ def main():
             # Slack variable for demand
             var_dict['y-' + item['id']] = m.addVar(vtype=GRB.CONTINUOUS)
             var_dict['y-' + item['id']].varName = 'y-' + item['id']
+            # Add bounds to breakpoints
+            item['breakpoints'].insert(0, 0)
+            item['breakpoints'].append(1e6)
+            K = len(item['breakpoints'])
+            if K != len(item['marginal_costs']) + 1:
+                raise KeyError(f"Length of breakppoints and marginal costs "
+                               f"do not match for {item['name']}")
+            # SOS2 variable
+            sos_vars = []
+            for val in range(K):
+                s_id = 'z' + item['id'] + f"{val:02}"
+                var_dict[s_id] = m.addVar(vtype=GRB.CONTINUOUS)
+                var_dict[s_id].varName = s_id
+                sos_vars.append(var_dict[s_id])
+            m.addSOS(GRB.SOS_TYPE2, sos_vars)
         for item in machines:
             var_dict['k' + item['id']] = m.addVar(vtype=GRB.BINARY)
             var_dict['k' + item['id']].varName = 'k' + item['id']
-
-        # Some function definitions to help out with objective and constraints
-        def get_demand(i):
-            return(var_dict['x'+i] - (var_dict['y+'+i] - var_dict['y-'+i]))
 
         def get_volumes():
             vol = 0
@@ -56,29 +70,76 @@ def main():
                 cost += var_dict['k' + item['id']] * item['cost']
             return(cost)
 
-        def get_demand_slack_penalty():
+        def get_generic_penalties():
             penalty = 0
             for item in products:
-                penalty += var_dict['y-' + item['id']] * item['profit']
+                penalty += var_dict['y-' + item['id']] * item['penalty']
             return(penalty)
 
-        # Objective
-        mask_short_penalty = 50
-        m.setObjective(get_demand_slack_penalty()
-                       + get_machine_cost()
-                       + mask_short_penalty * var_dict['y-06'],
-                       GRB.MINIMIZE)
+        def get_profit(item):
+            breakpoints = item['breakpoints']
+            costs = item['marginal_costs']
+            profit = 0
 
-        # Constraints
+            def recursive_profit(x, ind):
+                if x <= breakpoints[ind + 1]:
+                    return(costs[ind] * (x - breakpoints[ind]))
+                else:
+                    p = recursive_profit(x, ind + 1)
+
+                if ind > 0:
+                    p += (costs[ind] * (breakpoints[ind+1] - breakpoints[ind]))
+                else:
+                    p += costs[ind] * breakpoints[ind + 1]
+                return(p)
+
+            for ind in range(len(breakpoints)):
+                s_id = 'z' + item['id'] + f"{ind:02}"
+                pf = recursive_profit(breakpoints[ind], 0)
+                profit += var_dict[s_id] * pf
+
+            return(profit)
+
+        """ Objectives """
+        # Machine capital cost
+        machine_cost = get_machine_cost() / alpha
+        # Short supply penalties
+        generic_penalties = get_generic_penalties()
+        # Profit loss due to short supply penalty
+        # TODO: Update model in report
+        # Removed original profit loss because model would set all item
+        # stocks to 0 to get a potential profit of 0 which means that no
+        # profit is lost by not stocking an item
+        profit = 0
+        for item in products:
+            profit += get_profit(item)
+
+        m.setObjective(machine_cost + generic_penalties - profit)
+        """ Constraints """
         # Demand
         for item in products:
-            m.addConstr(get_demand(item['id']) == float(item['demand']),
-                        "demand"+item['id'])
+            m.addConstr(var_dict['x'+item['id']] - item['demand'] -
+                        (var_dict['y+'+item['id']]-var_dict['y-'+item['id']])
+                        == 0)
         # Volume
         m.addConstr(get_volumes() - get_max_volume() <= 0, 'volume')
         # One Machine
         m.addConstr(get_machines() == 1, 'single kiosk')
+        # SOS2 Equations
+        for item in products:
+            sum_sos2 = 0
+            var_eqn = 0
+            for val in range(len(item['breakpoints'])):
+                s_id = 'z' + item['id'] + f"{val:02}"
+                sum_sos2 += var_dict[s_id]
+                var_eqn += var_dict[s_id] * item['breakpoints'][val]
+            # Convexity equation
+            m.addConstr(sum_sos2 == 1, f"Convexity - {item['name']}")
+            # Variable equation
+            m.addConstr(var_eqn - var_dict['x' + item['id']] == 0,
+                        f"Variable Eqution - {item['name']}")
         # Optimize
+
         m.optimize()
 
         # Print results
